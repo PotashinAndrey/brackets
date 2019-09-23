314524d203dcc91280c8a33b72e50ccd54d756d6
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2019-09-23 20:21:11 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let buffer = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (searchInBracketsConfig(str[i], bracketsConfig, buffer) == false) {
+      return false;
+    }
+  }
+
+  if (buffer.length != 0) {
+      return false;
+  }
+
+  return true;
+}
+
+function searchInBracketsConfig(symbol, bracketsConfig, buffer) {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+
+
+    if (bracketsConfig[i][1] == symbol && buffer[buffer.length - 1] == bracketsConfig[i][0]) {
+        buffer.pop();
+        break;
+      }
+
+    if (bracketsConfig[i][0] == symbol) {
+      buffer.push(symbol);
+      break;
+    }
+
+
+    if (bracketsConfig[i][1] == symbol && buffer[buffer.length - 1] != bracketsConfig[i][0]) {
+      return false;
+    }
+  }
 }
